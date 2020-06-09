19b65bd61a19fcbcf0349ee15116269ad57c5acd
JieBrother <1030159799@qq.com>
2020-06-09 15:14:40 +0800

    软件工程第五次实验-Eclipse集成git并完成项目提交到远程仓库

---
+++ src/main/java/com/example/demo/BookDao.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class BookDao {
+
+	public void addBook(int id,String bookname) {
+		//添加操作
+	}
+	
+	public void deleteBook(int id) {
+		//删除操作
+	}
+	
+	public void updateBook(int id,String newBookName) {
+		//修改操作
+	}
+	
+	public List<Object> scanBook(int pageIndex,int pageSize){
+		//查询操作
+		return null;
+	}
+	
+}
